66d0c504333b10dc1d5bf17e5d903613963438e6
osvaldofor97 <111823620+osvaldofor97@users.noreply.github.com>
2022-08-27 18:38:25 -0500

    Initial commit

---
+++ src/main/java/com/vuelafacil/VuelafacildemoApplication.java
@@ -0,0 +1,13 @@
+package com.vuelafacil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VuelafacildemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VuelafacildemoApplication.class, args);
+	}
+
+}
