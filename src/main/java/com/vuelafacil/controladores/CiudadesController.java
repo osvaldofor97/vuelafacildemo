9e9f20ca1f61178f35db1745e60da4a25cb08580
osvaldofor97 <111823620+osvaldofor97@users.noreply.github.com>
2022-09-11 18:04:16 -0500

    Mejora de la pagina Web listados, formularios y mejora visual

--- src/main/java/com/vuelafacil/controladores/CiudadesController.java
+++ src/main/java/com/vuelafacil/controladores/CiudadesController.java
@@ -1,8 +1,15 @@
 package com.vuelafacil.controladores;
 
+import com.vuelafacil.entidades.ciudades;
+import com.vuelafacil.servicios.ciudadService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
 * index.html            /
@@ -15,18 +22,47 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class CiudadesController {
     
+    @Autowired
+    private ciudadService servicio;
+    
     @GetMapping("/")
     public String cargarIndex(){
         return "index";
     }
     
     @GetMapping("/ciudades")
-    public String cargarCiudades(Model modelo){
-        return "listaCiudades";
+    public String cargarCiudades(Model modelo, @RequestParam(value="criterio",required=false) String criterio){
+        if (criterio == null) {
+            modelo.addAttribute("ciudades",servicio.consultarCiudad());
+        }else{
+            modelo.addAttribute("ciudades",servicio.consultarCiudad(criterio));
+        }
+        return "listaciudades";
     }
     
     @GetMapping("/ciudades/form")
-    public String cargarFormCiudades(){
-        return "formCiudades";
+    public String cargarFormCiudades(Model modelo){
+        modelo.addAttribute("ciudad", new ciudades());
+        return "formciudades";
+    }
+    
+    @GetMapping("/ciudades/form/{idciudades}")
+    public String cargarFormCiudades(Model modelo, @PathVariable int idciudades){
+        ciudades c = servicio.consultarCiudad(idciudades);
+        modelo.addAttribute("ciudad",c);
+        return "formciudades";
+    }
+    
+    @PostMapping("/ciudades")
+    public String guardarCiudad(@ModelAttribute ciudades c){
+        servicio.crearCiudad(c);
+        return "redirect:/ciudades";
+    }
+    
+    @GetMapping("/ciudades/form/{idciudades}")
+    public String eliminarCiudad(@PathVariable int idciudades){
+        ciudades c = servicio.consultarCiudad(idciudades);
+        servicio.eliminarCiudad(c);
+        return "redirect:/ciudades";
     }
 }
